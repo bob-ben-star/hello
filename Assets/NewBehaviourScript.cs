253de90e00f5de06d5ac1dfd763cf9687484cff9
wangjishu <wangjishu@hongmoe.com>
2021-05-24 12:08:08 +0800

    修改脚本

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("123");
     }
 
     // Update is called once per frame
